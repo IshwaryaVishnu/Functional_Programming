6f6a6e71f764aec5c3e63a5f08ddd9fd171a8d7b
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-11-29 11:50:31 +0100

    Anonymous Inner class

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -3,12 +3,25 @@ package se.lexicon;
 import se.lexicon.interfaces.DoStringStuff;
 import se.lexicon.interfaces.integerOperator;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-    }
+//MovingAnonymous Inner classes to fields
+public class App {
+     static DoStringStuff concatOperator = new DoStringStuff() {
+        @Override
+        public String operator(String s1, String s2) {
+            return s1.concat(s2);
+        }
+    };
+   static integerOperator addition = new integerOperator() {
+        @Override
+        public Integer apply(Integer n1, Integer n2) {
+            return n1 + n2;
+        }
+    };
 
+    public static void main( String[] args )
+    {exe3();
+    }
+/*
     //Anonymous Inner class
     public  static void exe1(){
         DoStringStuff concatOperator = new DoStringStuff() {
@@ -23,6 +36,13 @@ public class App
                 return n1 + n2;
             }
         };
+        }
+*/
+    public static void exe3(){
+        System.out.println(concatOperator.operator("Iswarya","Yaalan"));
+        System.out.println(addition.apply(1,3));
+            }
+        }
 
-}
-}
+
+
